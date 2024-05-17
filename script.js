c14e2b9d7f121189a5368e574fb190b860a02098
ibragim567 <ibragim.almamedov01@mail.ru>
2024-05-18 00:52:48 +0400

    Feat: If Else Task

---
+++ script.js
@@ -0,0 +1,97 @@
+//Write a program that determines whether a given number is positive or negative.
+let positiveNum = 1
+let negativeNum = -1
+
+if (positiveNum >0) {
+    console.log('it is positive number');
+}  else {
+console.log('it is negative number');
+} 
+
+if (negativeNum <0) {
+    console.log('it is negative number');
+}  else {
+console.log('it is positive number');
+} 
+
+//Write a program that checks if a number is even or odd.
+
+const number = prompt("Enter a number: ");
+
+if(number % 2 == 0) {
+    console.log("The number is even.");
+}
+
+else {
+    console.log("The number is odd.");
+}
+
+
+//Write a program to determine the greater of two numbers.
+
+let A = 10 
+let B = 5  
+
+if (A > B) {
+    console.log("A is greater than B");
+} else if (A < B) {
+    console.log("B is greater than A");
+} else {
+    console.log("A and B are equal");
+}
+
+//Write a program that assigns a letter grade based on a numerical grade.
+
+let score = 85;
+let letter;
+
+if (score > 90) {
+    letter = 'A';
+}
+else if (score > 80) {
+    letter = 'B';
+}
+else if (score > 70) {
+    letter = 'C'
+}
+else if (score > 60) {
+    letter = 'D'
+}
+else {
+    letter = 'F'
+}
+console.log(letter);
+
+
+
+//Write a program that calculates the ticket price based on age.
+
+let year = 18
+let ticketprice;
+
+if (year>18 && year <20) {
+    ticketprice = '25'
+}
+else if (year>20 && year <25 ) {
+    ticketprice = '20'
+}
+else if (year>25 && year <30) {
+    ticketprice = '15'
+}
+else if (year>31 && year <35) {
+    ticketprice = '10'
+}
+else {
+    ticketprice = '5'
+}
+console.log(ticketprice);
+
+
+//Write a program that calculates a discount based on the purchase amount.
+//Bu sualda fucntion yazmaq lazim idi deye bos buraxdim
+  
+  
+
+//Write a simple number guessing game.
+//Bu sualda fucntion yazmaq lazim idi deye bos buraxdim
+
